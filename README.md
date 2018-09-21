c19bcb746a1c6ac856ec222e548d84c760f8de38
Alessio Innocenzi <ainnocenzi95@gmail.com>
2018-09-21 09:31:54 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # SwipeableMultiselect
 A jquery plugin to make multiple selection on a bootstrap btn-group in an easier way
+
+## Details
+The Plugin allows you to make single or multiple selection. You can also press mouse and drag the cursor on other options to make selection in a faster way
+
+## Dependencies
+Swipeable Multiselect requires JQuery and Bootstrap 3. The plugin will work also with Bootstrap 4, but since Bootstrap has modified a lot the CSS from v3 to v4, the style of the plugin won't be the same.
+
+## Usage:
+
+```
+$(document).ready(function(){
+
+    $("#swipeable-basic").swipeableMultiselect({
+        values: [{ "text": "Apple", "value": "apple"}, { "text": "Pear", "value": "pear" }, { "text": "Banana", "value": "banana" }]
+    }); //basic usage
+
+
+    $("#swipeable-radio").swipeableMultiselect({
+        values: [{ "text": "Apple", "value": "apple"}, { "text": "Pear", "value": "pear" }, { "text": "Banana", "value": "banana" }],
+        allowMultiple: false
+    }); //radio selection (if you want only one option can be selected)
+
+
+    $("#swipeable-active").swipeableMultiselect({
+        values: [{ "text": "Apple", "value": "apple", "active": true }, { "text": "Pear", "value": "pear" }, { "text": "Banana", "value": "banana" }]
+    }); //initialization with custom active option (you can set the flag active:true and the current option will be the active option after initialization)
+});
+```
+
+## Methods
+
+`GetSelectedValue`: returns an Array of the group allows multiple selection, String otherwise
+`SetActive`: set active option by value dinamically after initialization. If the group allows multiple selection the method requires an Array, String otherwise
+
+```
+$(document).ready(function(){
+
+    $("#swipeable-basic").swipeableMultiselect("GetSelectedValue"); //returns ["apple", "pear"]
+
+
+    $("#swipeable-radio").swipeableMultiselect("GetSelectedValue"); //returns "apple"
+
+
+    /* set active value dinamically after initialization */
+    $("#swipeable-basic").swipeableMultiselect("SetActive", ["apple"]); 
+
+
+    $("#swipeable-radio").swipeableMultiselect("SetActive", "apple");
+});
+```
---
+++ SwipeableMultiselect.js
@@ -0,0 +1,150 @@
+﻿(function ($) {
+
+    var getValues = function (actives) {
+        var values = [];
+        actives.each(function () {
+            values.push(this.childNodes[0].value);
+        });
+        return values;
+    };
+
+    var getValue = function (actives) {
+        return actives[0].childNodes[0].value;
+    };
+
+    var setValue = function (labels, val) {
+        labels.each(function () {
+            if (val == this.childNodes[0].value)
+                $(this).addClass('active');
+        });
+    };
+
+    var setValues = function (labels, val) {
+        labels.each(function () {
+            for (var v of val) {
+                if (v == this.childNodes[0].value)
+                    $(this).addClass('active');
+            }
+        });
+    };
+
+    var methods = {
+        Init: function (options) {
+            var values = options.values;
+            var allowmultiple = options.allowMultiple;
+            var groupid = $(this).attr("id");
+
+            $(this).wrap("<div class=\"btn-group\" id=\"" + groupid + "\" data-toggle=\"buttons\">");
+            
+            var optsHtmlString = "";
+            var inputType, isActive;
+
+            var custom_border_class = "";
+
+            var index = 0;
+            for (var opt of values) {
+                if (allowmultiple==false) inputType = "radio";
+                else inputType = "checkbox";
+                if (opt.active != null && opt.active == true) isActive = "active";
+                else isActive = "";
+                
+                if (index == 0) custom_border_class = "first-option";
+                else if (index == values.length - 1) custom_border_class = "last-option";
+                else custom_border_class = "";
+
+                if (opt.image == null) {
+                    optsHtmlString += "<label class=\"multiselect-option " + isActive + " btn btn-sm btn-primary " + custom_border_class + "\"><input value=\"" + opt.value + "\" type=\"" + inputType + "\" autocomplete=\"off\">" + opt.text + "</label>";
+                }
+                else {
+                    optsHtmlString += "<label class=\"multiselect-option " + isActive + " btn btn-sm btn-primary " + custom_border_class + "\"><input value=\"" + opt.value + "\" type=\"" + inputType + "\" autocomplete=\"off\"><img value=\"" + opt.value + "\" src=\"" + opt.image + "\" width=\"50px;\" height=\"50px;\" /></label>";
+                }
+                index++;
+            }
+
+            $(this).replaceWith(optsHtmlString);
+
+            var mousedownOn = {
+                el: null
+            };
+
+            $(document)
+                .mouseup(function () {
+                    mousedownOn.el = null;
+                });
+
+            $("#" + groupid + " .multiselect-option")
+                .mousedown(function () {
+                    var $this = $(this);
+                    mousedownOn.el = $(this);
+                    $this.button('toggle');
+                })
+                .mouseenter(function () {
+                    var $this = $(this);
+                    if (mousedownOn.el != null && $this.hasClass('active') != mousedownOn.el.hasClass('active')) {
+                        $this.button('toggle', true);
+                    }
+                })
+                .click(function (e) {
+                    e.preventDefault();
+                    return false;
+                });
+        },
+        GetSelectedValue: function() { //$("#platform-swipeable").swipeableMultiselect("GetSelectedValue");
+            var $this = $(this);
+            var actives = $this.find("label.active");
+            var result = undefined;
+            if (actives.length > 0) {
+                var swipeableType = actives[0].childNodes[0].type;
+                if (swipeableType == "checkbox") {
+                    result = getValues(actives);
+                }
+                if (swipeableType == "radio") {
+                    result = getValue(actives);
+                }
+            }
+
+            return result;
+        }, 
+        SetActive: function (val) {
+            var $this = $(this);
+            var actives = $this.find("label.active");
+
+            actives.each(function () {
+                $(this).removeClass('active');
+            });
+
+            var labels = $this.find("label");
+            
+            var swipeableType = labels[0].childNodes[0].type;
+            if (swipeableType == "checkbox") {
+                if (val.constructor !== Array) {
+                    $.error("Error! You are trying to call SetActive function passing only one value but the control is configured to accept multiple values. Please retry passing an Array of values.");
+                }
+                else {
+                    setValues(labels, val);
+                }
+            }
+            if (swipeableType == "radio") {
+
+                if (val.constructor === Array) {
+                    $.error("Error! You are trying to call SetActive function passing and Array but the control is configured to accept only one value. Please retry passing only one value.");
+                }
+                else {
+                    setValue(labels, val);
+                }
+            }
+        }
+    };
+
+    $.fn.swipeableMultiselect = function (methodOrOptions) {
+        if (methods[methodOrOptions]) {
+            return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            return methods.Init.apply(this, arguments);
+        } else {
+            $.error('Method ' + methodOrOptions + ' does not exist on jQuery.swipeableMultiselect');
+        }
+    };
+
+
+})(jQuery);
